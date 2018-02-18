fb649ea84e0edac31039c56a7f8706a6b2132ca8
DESKTOP-Q15IRBB\Slavarenik <SlavarenikFlance@gmail.com>
2018-02-18 23:46:41 +0300

    Шаблонизатор ejs

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 var express = require('express');
-
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-  res.send('This is home');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/news', function(req, res){
-  res.send('This is news');
+  res.sendFile(__dirname + '/about.html');
 });
 
 app.get('/news/:id', function(req, res){
-  res.send('ID is - ' + req.params.id);
+  var obj = {
+    title: 'News',
+    id: 4,
+    par: ['Paragraph', 'Regular text', 'Numbers: 2, 4, 6', '99']
+  };
+  res.render('news', {
+    newsId: req.params.id,
+    obj: obj
+  });
 });
 
 app.listen(3000);
