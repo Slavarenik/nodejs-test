5a4a26bfcf1b8e1450d37731b21173cda3428546
DESKTOP-Q15IRBB\Slavarenik <SlavarenikFlance@gmail.com>
2018-02-19 00:48:27 +0300

    Form data, static files

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use('/public', express.static('public'));
+
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.render('index');
 });
 
-app.get('/news', function(req, res){
-  res.sendFile(__dirname + '/about.html');
+app.get('/about', function(req, res){
+  res.render('about');
 });
 
 app.get('/news/:id', function(req, res){
@@ -21,6 +26,12 @@ app.get('/news/:id', function(req, res){
     newsId: req.params.id,
     obj: obj
   });
+});
+
+app.post('/about', urlencodedParser, function(req, res){
+  if (!req.body) return res.sendStatus(400);
+  console.log(req.body);
+  res.render('about-succes', {data: req.body});
 });
 
 app.listen(3000);
